9b986c39b5f89889f297c7a6b3ef9282c1a52d05
omarmaged01 <150797066+omarmaged01@users.noreply.github.com>
2024-05-09 22:48:50 +0300

    Delete KeyReqMsg.cc

--- KeyReqMsg.cc
+++
@@ -1,8 +0,0 @@
-// KeyReqMsg.cc
-#include "KeyReqMsg.h"
-
-// Implement the constructor
-KeyReqMsg::KeyReqMsg(const char *name, int kind) : omnetpp::cMessage(name, kind) {}
-
-// Implement the destructor
-

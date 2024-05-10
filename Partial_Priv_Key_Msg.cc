8a4e33ee4abe4227d8b865b632d21ead15369188
omarmaged01 <150797066+omarmaged01@users.noreply.github.com>
2024-05-10 12:46:13 +0300

    Add files via upload

---
+++ Partial_Priv_Key_Msg.cc
@@ -0,0 +1,5 @@
+#include "Partial_Priv_Key_Msg.h"
+
+Partial_Priv_Key_Msg::Partial_Priv_Key_Msg(const char *name, int kind) : cMessage(name, kind) {}
+
+Partial_Priv_Key_Msg::~Partial_Priv_Key_Msg() {}

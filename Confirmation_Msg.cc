f5c79f22b2ae72df7bf832bff11194b3db21156d
Karam18 <150797028+Karam18@users.noreply.github.com>
2024-05-10 12:48:00 +0300

    Add files via upload

---
+++ Confirmation_Msg.cc
@@ -0,0 +1,17 @@
+#include "Confirmation_Msg.h"
+
+Confirmation_Msg::Confirmation_Msg(const char *name, int kind) : cMessage(name, kind) {
+    // Initialize any other members if needed
+}
+
+Confirmation_Msg::~Confirmation_Msg() {
+    // Perform any cleanup if needed
+}
+
+
+
+
+
+
+
+

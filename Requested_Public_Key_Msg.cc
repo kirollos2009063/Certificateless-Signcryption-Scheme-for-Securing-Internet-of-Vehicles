937f1141a6236115be64e0b125164b0000e99f70
Mariamelsawahh <150798644+Mariamelsawahh@users.noreply.github.com>
2024-05-10 13:33:31 +0300

    Add files via upload

---
+++ Requested_Public_Key_Msg.cc
@@ -0,0 +1,11 @@
+#include "Requested_Public_Key_Msg.h"
+
+Requested_Public_Key_Msg::Requested_Public_Key_Msg(const char *name, int kind) : cMessage(name, kind) {
+    // Initialize any other members if needed
+}
+
+Requested_Public_Key_Msg::~Requested_Public_Key_Msg() {
+    // Perform any cleanup if needed
+}
+
+
